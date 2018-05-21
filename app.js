44436d77099438b5a888e32862b07c51e5abc888
Lokesh Cherukuri <lok.cherukuri@gmail.com>
2018-05-21 10:13:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require('request');
 googlehome.ip('192.168.86.55', 'en');
 googlehome.device('Lokesh.GoogleHome');
 
-googlehome.notify("Hi Lokesh! Testing 1 2 3", function(res) { });
+googlehome.notify("Hi there! Testing 1 2 3", function(res) { });
 
 function run() {
   request('https://api.coinbase.com/v2/prices/USD/spot', { json: true }, (err, res, body) => {
@@ -14,7 +14,7 @@ function run() {
     }
     var btc = body.data[0];
     var btcPrice = Math.floor(btc.amount);
-    var message = 'Hi Lokesh! The price of Bitcoin is $'+ btcPrice + '. I repeat $'+ btcPrice;
+    var message = 'Hi there! The price of Bitcoin is $'+ btcPrice + '. I repeat $'+ btcPrice;
     googlehome.notify(message, function(res) { });
   });
 }
